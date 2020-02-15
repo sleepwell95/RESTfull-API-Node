9d4284f368b54e743bbdfefd331b20e405e92ae4
Tomas Kelečius <sleepwell95@gmail.com>
2020-02-15 15:40:48 +0200

    Updated app.js file

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
 
-
+app.use('/orders', ordersRoutes);
 app.use('/products', productRoutes);
 
 
