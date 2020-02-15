ec50c8ab256672b225a3f5f585d14c8c73ab9564
Tomas Kelečius <sleepwell95@gmail.com>
2020-02-15 14:34:14 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+
+
+app.use('/products',productRoutes);
+
+
+module.exports = app;
