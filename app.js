46a850f622b05cfff1ef441aaefdf4dbae0cb084
Tomas Kelečius <sleepwell95@gmail.com>
2020-02-15 14:37:31 +0200

    Example for Matas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
+const app2 = 50;
 const productRoutes = require('./api/routes/products');
 
 
-app.use('/products',productRoutes);
+app.use('/products', productRoutes);
 
 
 module.exports = app;
