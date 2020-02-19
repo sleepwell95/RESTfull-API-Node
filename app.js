106de199970f060e3896b3d9b4e9129297108bf8
Tomas Kelečius <sleepwell95@gmail.com>
2020-02-19 14:07:47 +0200

    Added CORS Error Handling and Access control

--- app.js
+++ app.js
@@ -11,8 +11,25 @@ app.use(morgan('dev'));
 
 //? Adding body parsing
 // Extracts JSON data and makes it easily readable
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
+
+
+// CORS Access control, preventing CORS errors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({
+
+        });
+    }
+    next();
+});
+
 
 //Routes for requests handling
 app.use('/orders', ordersRoutes);
@@ -21,19 +38,19 @@ app.use('/products', productRoutes);
 //!   Error handling functions
 // Sends Not found or 404 error to console
 app.use((req, res, next) => {
-  const error = new Error('Not FOUND!');
-  error.status = 404;
-  next(error);
+    const error = new Error('Not FOUND!');
+    error.status = 404;
+    next(error);
 });
 
 // Returns specific JSON in console
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
 });
 
 module.exports = app;
